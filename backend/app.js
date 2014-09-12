595c8827afd3e892dcfd41c78d19a33051f62e15
FND <fnd@localhost.localdomain>
2014-09-12 09:52:48 +0200

    tweaked URIs
    in preparation for form submissions

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,26 @@ nunjucks.configure("templates", { autoescape: true, express: app });
 app.use("/assets", express.static(path.join(__dirname, "..", "assets")));
 
 app.get("/", function(req, res) {
+	var flightNumber = rand(100, 9999);
+	res.redirect("/check-in/rc-" + flightNumber);
+});
+
+app.get("/check-in/:flight", function(req, res) {
+	var flightID = req.params.flight.toUpperCase();
+
+	if(flightID.indexOf("RC-") !== 0) { // someone's being clever
+		res.status(404).send("We're afraid flight " + flightID +
+				" is not operated by ROCAir.");
+		return;
+	}
+
 	res.render("seats.html", {
-		title: app.get("title")
+		title: app.get("title"),
+		flightID: flightID
 	});
 });
+
+function rand(min, max) {
+	var num = Math.random() * (max - min) + min;
+	return Math.floor(num);
+}
